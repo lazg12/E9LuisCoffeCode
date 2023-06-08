03649659a8c9143122cc98315b3e5be981506105
lazg1 <lazg12@gmail.com>
2023-06-08 14:32:04 -0500

    Inicio de la clase 6

---
+++ src/clase006/Main.java
@@ -0,0 +1,9 @@
+package src.clase006;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a la clase 6");
+    }
+}
